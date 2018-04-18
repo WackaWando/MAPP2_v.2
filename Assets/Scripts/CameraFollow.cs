d4a606df57e782ba94d244ec58ffff1ef863fcd6
WackaWando <37776964+WackaWando@users.noreply.github.com>
2018-04-18 18:14:16 +0200

    FixedUpdate Camera movement
    istället för i Update() metoden

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,34 @@ public class CameraFollow : MonoBehaviour {
     public Vector3 distanceFromPlayer;  //default value x:0, y:1.5, z: -4   set in inspector.
 
 
+    private bool passedFinishLine;
+    private float playerPositionZ;
 
+    void Start()
+    {
+        passedFinishLine = false;
+    }
 
+    private void FixedUpdate()
+    {
+        playerPositionZ = player.position.z;
+        if (playerPositionZ > 850)
+        {
+            passedFinishLine = true;
+        }
 
-	void Update () {
+        if (passedFinishLine == false)
+        {
+            transform.position = player.position + distanceFromPlayer;
+        }
+    }
+    void Update () {
 
-        transform.position = player.position + distanceFromPlayer;
-	}
+       
+            
+        
+        
+    }
+
+    
 }

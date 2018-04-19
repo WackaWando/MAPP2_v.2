07e2fabbe1c634219da8cbaf7f17fa207d63fca4
Kan <atthasit.khan@gmail.com>
2018-04-19 20:51:12 +0200

    ObjectRotate script+tomat pref

---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour {
+
+    public int z;
+    public int x;
+    public int y;
+  
+  
+    void Update()
+    {
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+    }
+
+
+}

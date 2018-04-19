950442bd0c7d22bae5a36d6bdb9068bc25b80bb2
Kan <atthasit.khan@gmail.com>
2018-04-19 20:22:54 +0200

    Camera Smoothing

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+    public Transform PlayerTranform;
+    private Vector3 _cameraOffset;
+    [Range(0.01f,1.0f)]
+    public float SmoothFactor = 0.5f;
+    public bool LookAtPlayer = false;
+	// Use this for initialization
+	void Start () {
+        _cameraOffset = transform.position - PlayerTranform.position;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Vector3 newPos = PlayerTranform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position,newPos,SmoothFactor);
+        if (LookAtPlayer) {
+            transform.LookAt(PlayerTranform);
+        }
+    }
+}

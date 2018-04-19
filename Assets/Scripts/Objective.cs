b2268a9207865d6d6d13d074862e3d3aa44393f9
Kan <atthasit.khan@gmail.com>
2018-04-19 20:41:32 +0200

    collecting script+pref

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour {
+
+    
+    public GameObject Paricle;
+    // Use this for initialization
+    void Start()
+    {
+            Paricle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Vector3 spawn = transform.position;
+            Paricle.transform.position = spawn;
+            Paricle.SetActive(true);
+            gameObject.SetActive(false);
+            
+        }
+
+    }
+}

e87c3df5ee455a314f531560d6c69229e8481ad7
mouta-a <moutasem.a.h@live.se>
2018-04-23 10:30:40 +0200

    Fixed width script

---
+++ Assets/Scripts/FixedWidth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class FixedWidth : MonoBehaviour
+    {
+    //This is the field of view that the Camera has
+    public float horizontalFOV = 75f;
+
+        void Start()
+        {
+       
+    }
+
+        void Update()
+        {
+        GetComponent<Camera>().fieldOfView = calcVerticalFOV(horizontalFOV, GetComponent<Camera>().aspect);
+
+
+    }
+
+    private float calcVerticalFOV(float horizontalFOV, float aspectRatio)
+    {
+        float hFOVInRads = horizontalFOV * Mathf.Deg2Rad;
+        float vFOVInRads = 2 * Mathf.Atan(Mathf.Tan(hFOVInRads / 2) / aspectRatio);
+        float vFOV = vFOVInRads * Mathf.Rad2Deg;
+        return vFOV;
+    }
+
+    }

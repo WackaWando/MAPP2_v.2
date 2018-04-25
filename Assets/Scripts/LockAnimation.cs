c785338daa830e33dc198c1daa6c7ca009fb157b
Kan <atthasit.khan@gmail.com>
2018-04-25 21:17:27 +0200

    AnimationControll

---
+++ Assets/Scripts/LockAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockAnimation : MonoBehaviour {
+
+    public Rigidbody rigBod;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FreezePoop() /*Function used in Animation Event where all constraints for the Rigidbody2D are frozen.*/
+    {
+        rigBod.constraints = RigidbodyConstraints.FreezePosition;
+    }
+
+    void UnfreezePoop() /*Function used in Animation Event where all constraints for the Rigidbody2D are unfrozen.*/
+    {
+        rigBod.constraints = RigidbodyConstraints.None;
+    }
+}

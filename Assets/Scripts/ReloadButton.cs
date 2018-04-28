5c034a8557c5a2ea3dc5045bcf6d9ee0ef54ec74
jokerman54 <37561700+jokerman54@users.noreply.github.com>
2018-04-28 13:40:27 +0200

    build settings

---
+++ Assets/Scripts/ReloadButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadButton : MonoBehaviour {
+
+    public void ReloadScene () {
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene("Test_Level");
+}
+
+// Use this for initialization
+void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

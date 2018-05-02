956270466bf25b59f83ad5a4eb34fcd9d06f04cd
WackaWando <37776964+WackaWando@users.noreply.github.com>
2018-05-02 16:23:30 +0200

    trafficlightbaby
    yeaboi

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CountDown : MonoBehaviour {
-    private float timer = 4f;
-    public Text text;
+    private float timer = 3f;
+    
     public Transform player;
+    public GameObject red, yellow, green;
+    
 
     void Start () {
         player.GetComponent<DragPlayer>().SetForwardForce(false);
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         timer -= Time.deltaTime;
-        text.text = "" + (int)timer;
+       
         if (timer<0)
         {
-            text.enabled = false;
+            
             player.GetComponent<DragPlayer>().SetForwardForce(true);
+            yellow.SetActive(false);
+            green.SetActive(true);
+            red.SetActive(false);
         }
+        else if (timer < 1.5)
+        {
+           
+            yellow.SetActive(true);
+        }
+
 
 	}
 }

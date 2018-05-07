5abdff4dec3011c668f610e9cc9511654f09cb15
Kan <atthasit.khan@gmail.com>
2018-05-07 20:01:49 +0200

    update rotation kniv

--- Assets/Scripts/ObjectRotation.cs
+++ Assets/Scripts/ObjectRotation.cs
@@ -7,12 +7,21 @@ public class ObjectRotation : MonoBehaviour {
     public int z;
     public int x;
     public int y;
-  
-  
+  public bool bounce;
+    public int bounceTime;
+    public int speed = 2;
+
+    
     void Update()
     {
-        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+        
+        if (bounce)
+        {
+            transform.rotation = Quaternion.Euler(x, y, bounceTime * Mathf.Sin(Time.time * speed));
+        }
+        else
+            transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
     }
 
-
+    
 }

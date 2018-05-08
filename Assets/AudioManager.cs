e5e5fac00cd2506659497e19261d84d439383ce6
mouta-a <moutasem.a.h@live.se>
2018-05-08 11:51:52 +0200

    audiomanager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sounds[] sounds;
+	// Use this for initialization
+	void Awake () {
+		foreach(Sounds s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+	}
+	
+    public void Play(string name)
+    {
+        Sounds s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+           
+        s.source.Play();
+    }
+}

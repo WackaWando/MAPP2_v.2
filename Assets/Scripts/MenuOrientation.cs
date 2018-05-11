b2992d7bd8aefce06b882984d9a2d191e6f79208
jokerman54 <37561700+jokerman54@users.noreply.github.com>
2018-05-11 12:09:16 +0200

    Orientation
    levelorientation to be added in every level

---
+++ Assets/Scripts/MenuOrientation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOrientation : MonoBehaviour {
+    public Camera cam;
+
+    void Update()
+    {
+        if (Screen.height < Screen.width)
+        {
+            cam.GetComponent<FixedWidth>().SetFOV(116);
+
+        }
+        else
+        {
+            cam.GetComponent<FixedWidth>().SetFOV(75);
+        }
+    }
+}

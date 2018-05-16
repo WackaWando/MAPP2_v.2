4ee92711c76588d298f03845725a1995a024c931
jokerman54 <37561700+jokerman54@users.noreply.github.com>
2018-05-16 13:59:30 +0200

    temp prefab and the script needed

---
+++ Assets/Scripts/Veg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veg : MonoBehaviour {
+
+	void Start () {
+        PlayerPrefs.SetInt("veg", 1); // to be deleted, option to be added in main menu
+        if (PlayerPrefs.GetInt("veg", 0) == 0)
+        {
+            GameObject.Destroy(transform.GetChild(2).gameObject);
+        }
+        else
+        {
+            GameObject.Destroy(transform.GetChild(1).gameObject);
+        }
+
+    }
+	
+}
